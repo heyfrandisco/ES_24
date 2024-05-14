1ab51e9b337a56efc22623eb5d3dfdc7eeea82c5
fcallanam <francisco.callanam@gmail.com>
2024-05-14 14:54:04 +0100

    Connected to postgres db

--- physiot/settings.py
+++ physiot/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'physiot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'pass420',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
