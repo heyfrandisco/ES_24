df8c9a37c1581477b6c23405145313a56d0a823f
fcallanam <francisco.callanam@gmail.com>
2024-05-20 15:33:59 +0100

    removed old files

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from. import views
-
-urlpatterns = [
-    path('getusers/', views.getUsers),
-    path('adduser/', views.addUser),
-    path('adddoctor/', views.addDoctor),
-    path('addappointment/', views.addAppointment),
-]
